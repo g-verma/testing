10d305e3ea02a7b80488417c39543b15de2a6ec0
GV <g.verma0411@gmail.com>
2022-05-08 01:07:10 +0530

    Update queries.js
    added pagination query

--- client/lib/queries.js
+++ client/lib/queries.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client'
 export const GET_USERS = gql`
-query Query {
-  users {
+query Query($offset: Int, $limit:Int)  {
+  users (offset: $offset, limit:$limit){
     id
     name
     lastName
